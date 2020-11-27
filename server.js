4582ceef73032639718d0c337e00d0fa75d3d922
unknown <vicky.electron831@gmail.com>
2020-11-28 02:36:04 +0530

    added server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Enter me");
+const app = require('express');
+var port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
