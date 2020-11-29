4a7e52fb62a86071200416131260eae6ae84a1ea
unknown <vicky.electron831@gmail.com>
2020-11-29 05:39:05 +0530

    updated discount api

--- routers/discount.js
+++ routers/discount.js
@@ -2,16 +2,21 @@ import express from 'express';
 import axios from 'axios';
 var discountRouter = express.Router();
 discountRouter.get("/",function(req,res){
- axios.get('https://bgs-app.myshopify.com/admin/api/2020-10/price_rules.json',{
-headers:{'content-type': 'application/x-www-form-urlencoded'},    
- auth:{
-        username:'a1ee8df563789e623b68c2c0427e7e53',
-        password:'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'
-     }
- }).then(function(result){
+
+axios({
+    method: 'get',
+    url: 'https://bgs-app.myshopify.com/admin/api/2020-10/price_rules.json',
+    auth: {
+      username: 'a1ee8df563789e623b68c2c0427e7e53',
+      password: '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'
+    },
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+  }).then(function(result){
+    result = JSON.parse(JSON.stringify(result.data)),
     res.send(result);
     console.log(result);
- })
+ });
+
 
  
 
